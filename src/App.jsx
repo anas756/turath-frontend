a51aea49ba79e4508c53f418d9493262bb70125c
Anas <anastislane@gmail.com>
2026-04-19 17:39:44 +0100

    Created the main Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from "react";
+// src/App.jsx
+import React from "react";
+import Navbar from "./Navbar";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/posts")
-      .then(res => res.json())
-      .then(data => setPosts(data));
-  }, []);
-
   return (
     <div>
-      <h1>Posts</h1>
-      {posts.map(post => (
-        <div key={post._id}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-        </div>
-      ))}
+      <Navbar />
+      <h1>Welcome to my app</h1>
     </div>
   );
 }
