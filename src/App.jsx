f6e9b398d5b3e34be62f962a7620c0c303e530c6
simoatm234 <atmsimo599@gmail.com>
2026-04-29 14:05:04 +0100

    Initial commit (Clean Start)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to my app</h1>
+    </div>
+  );
+}
+
+export default App;
